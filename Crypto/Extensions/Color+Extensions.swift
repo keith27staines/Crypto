ffa6d17b95b6f6a154ab5a05115eedd13b1a67a9
Keith Staines <keith.staines@workfinder.com>
2021-10-28 18:33:26 +0100

    adding color theme

---
+++ Crypto/Extensions/Color+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Extensions.swift
+//  Crypto
+//
+//  Created by Keith Staines on 28/10/2021.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
---
+++ Crypto/Styling/ColorTheme.swift
@@ -0,0 +1,16 @@
+//
+//  ColorTheme.swift
+//  Crypto
+//
+//  Created by Keith Staines on 28/10/2021.
+//
+
+import SwiftUI
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
