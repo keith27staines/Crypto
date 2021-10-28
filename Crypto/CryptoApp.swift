37fbde910c6d314b6a1e4ff53b94b9ce73f5024f
Keith Staines <keith.staines@workfinder.com>
2021-10-28 18:17:20 +0100

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Keith Staines on 28/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
